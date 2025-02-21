ba53d49adfd54e554677d1e733ccd274f6b7ff19
Alvarito2203 <oviedoestevezalvaro@gmail.com>
2025-02-21 10:48:09 +0100

    comit 1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "tarea-facturas", appId: "1:999073656309:web:4ad2d2ed16d95c344904bc", storageBucket: "tarea-facturas.firebasestorage.app", apiKey: "AIzaSyAD6pMi5-NpjdDErUv_xkXqSzcl4mR0aWc", authDomain: "tarea-facturas.firebaseapp.com", messagingSenderId: "999073656309" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
